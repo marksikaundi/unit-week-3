6970c5cbbc35e4ebd7abc7d7c3e1bae01db4eff3
marksikaundi <emmanuelmark117@gmail.com>
2021-11-28 23:29:05 +0200

    added booleans and condition

---
+++ booleans.py
@@ -0,0 +1,16 @@
+5 == 5
+# true
+
+20 == 40
+#false
+
+#The == operator is one of the relational operators; the others are:
+#x != y # x is not equal to y
+#x > y # x is greater than y
+#x < y # x is less than y
+#x >= y # x is greater than or equal to y
+#x <= y # x is less than or equal to y
+
+#Although these operations are probably familiar to you, the Python symbols are different
+#from the mathematical symbols. A common error is to use a single equal sign (=) instead of
+#a double equal sign (==). Remember that = is a
---
+++ condition and recursion.py
@@ -0,0 +1,24 @@
+# floor division and modulus
+
+minutes = 105
+minutes / 60
+# 1.75
+
+minutes = 105
+hours = minutes // 60
+hours
+# 1
+
+remainder = minutes - hours * 60
+remainder
+# 45
+
+remainder = minutes / 60
+remainder
+# 45
+
+# conditional statements
+def x(args):
+    
+    if x > 0:
+        print('x is positive')
